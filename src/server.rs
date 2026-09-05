613a10f79c9290e2ca4a8dc319304acebead3cae
Arda Türkmenoğlu <ardatrkmnoglu@outlook.com>
2026-09-05 18:51:54 +0300

    feat: TCP listening from connection

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -9,5 +12,27 @@ impl Server {
 
     pub fn run(self) {
         println!("Server is listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buf = [0; 1024];
+                    match stream.read(&mut buf) {
+                        Ok(0) => println!("Connection closed by client."),
+                        Ok(n) => {
+                            println!(
+                                "Received request from {}: {}",
+                                addr,
+                                String::from_utf8_lossy(&buf[..n])
+                            );
+                        }
+                        Err(err) => println!("Failed to read from {}: {}", addr, err),
+                    }
+                }
+                Err(err) => println!("Failed to establish a connection: {}", err),
+            }
+        }
     }
 }
